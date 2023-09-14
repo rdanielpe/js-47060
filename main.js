cfb1c376014f44dfff5e6d9101ae089799db303d
Danielpe <Danielpe_24@hotmail.com>
2023-09-13 20:52:32 -0500

    ADD: conditional

--- main.js
+++ main.js
@@ -2,9 +2,18 @@ const cantPreguntas = Number(prompt("¿Cuantas notas deseas ingresar?"));
 let sumaNotas = 0;
 
 for (let i = 0; i < cantPreguntas; i++) {
-  const nota = Number(prompt("Ingrese la nota"));
-  sumaNotas += nota;
+ let nota
+  do {
+    const input = prompt("Ingrese la nota");
+    if (input === "" || isNaN(input)) {
+      alert("Por favor, ingrese un número válido.");
+    } else {
+      nota = Number(input);
+    }
+  } while (isNaN(nota));
+  sumaNotas = sumaNotas + nota
 }
+
 
 const promedio = sumaNotas / cantPreguntas;
 alert(`El promedio de las ${cantPreguntas} notas ingresadas es: ${promedio}`);

516ffb819c984d053ca430d8976572efe0af237c
Danielpe <Danielpe_24@hotmail.com>
2023-09-20 18:41:16 -0500

    ADD: create function validate

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
 const cantPreguntas = Number(prompt("¿Cuantas notas deseas ingresar?"));
 let sumaNotas = 0;
 
-for (let i = 0; i < cantPreguntas; i++) {
- let nota
-  do {
-    const input = prompt("Ingrese la nota");
-    if (input === "" || isNaN(input)) {
+function validarNota(input) {
+  let nota;
+  switch (true) {
+    case input === "":
       alert("Por favor, ingrese un número válido.");
-    } else {
+      break;
+    case isNaN(input):
+      alert("Por favor, ingrese un número válido.");
+      break;
+    default:
       nota = Number(input);
-    }
-  } while (isNaN(nota));
-  sumaNotas = sumaNotas + nota
+      break;
+  }
+  return nota;
 }
 
+for (let i = 0; i < cantPreguntas; i++) {
+  let nota;
+  do {
+    const input = prompt("Ingrese la nota");
+    nota = validarNota(input);
+  } while (isNaN(nota));
+  sumaNotas = sumaNotas + nota;
+}
 
 const promedio = sumaNotas / cantPreguntas;
 alert(`El promedio de las ${cantPreguntas} notas ingresadas es: ${promedio}`);
